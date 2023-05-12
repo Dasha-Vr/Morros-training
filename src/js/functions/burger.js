d813c21ed754d340a4530b92dd36850509d346c0
Dasha-Vr <110126844+Dasha-Vr@users.noreply.github.com>
2023-05-12 13:52:19 +0300

    Add files via upload

---
+++ src/js/functions/burger.js
@@ -0,0 +1,50 @@
+import { disableScroll } from '../functions/disable-scroll';
+import { enableScroll } from '../functions/enable-scroll';
+
+(function(){
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+  const line1 = document?.querySelector('[data-line1]');
+  const line2 = document?.querySelector('[data-line2]');
+  const line3 = document?.querySelector('[data-line3]');
+  const line4 = document?.querySelector('[data-line4]');
+  const menuItems = document?.querySelectorAll('[data-menu-item]');
+  const overlay = document?.querySelector('[data-menu-overlay]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('menu--active');
+    line1?.classList.toggle('line1--active');
+    line2?.classList.toggle('line2--active')
+    line3?.classList.toggle('line3--active')
+    line4?.classList.toggle('line4--active')
+
+    if (menu?.classList.contains('menu--active')) {
+      burger?.setAttribute('aria-expanded', 'true');
+      burger?.setAttribute('aria-label', 'Закрыть меню');
+      disableScroll();
+    } else {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      enableScroll();
+    }
+  });
+
+  overlay?.addEventListener('click', () => {
+    burger?.setAttribute('aria-expanded', 'false');
+    burger?.setAttribute('aria-label', 'Открыть меню');
+    burger.classList.remove('burger--active');
+    menu.classList.remove('menu--active');
+    enableScroll();
+  });
+
+  menuItems?.forEach(el => {
+    el.addEventListener('click', () => {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      burger.classList.remove('burger--active');
+      menu.classList.remove('menu--active');
+      enableScroll();
+    });
+  });
+})();
